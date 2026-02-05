18889258642d36b34fc756adb1490fff7afcc5ca
D1n0zaur <novozheev.cool@mail.ru>
2026-02-05 15:44:51 +0300

    huy v rot styles 2

--- app/routers/cms.py
+++ app/routers/cms.py
@@ -10,7 +10,7 @@ from app.dependencies import get_current_user
 from app.models.user import User
 from app.models.speciality import Speciality
 from app.models.feature import Feature
-from app.models.plan import Track, Direction, Discipline
+from app.models.plan import Direction, Discipline
 from app.models.teacher import Teacher
 from app.models.subject import Subject
 from app.models.achievement import Achievement
