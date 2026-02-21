cdc73ce8591e1e0c9e27caa4f89dd1c208a791c5
hll4 <nvinogradov721@gmail.com>
2026-02-21 20:07:03 +0300

    хуйня

--- app/main.py
+++ app/main.py
@@ -1,16 +1,14 @@
 import os
 import uvicorn
 from fastapi import FastAPI
-# StaticFiles больше не нужен в основном приложении, так как статику отдаст Caddy
-# from fastapi.staticfiles import StaticFiles 
+from fastapi.staticfiles import StaticFiles  # <-- 1. ВЕРНУЛИ ИМПОРТ
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 
-from app.routers import auth, public # cms убрали
+from app.routers import auth, public 
 from app.admin import setup_admin    
 
 app = FastAPI(title="IT BGITU Remake")
-
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key-change-me")
 app.add_middleware(
@@ -20,27 +18,12 @@ app.add_middleware(
     same_site="lax"
 )
 
-# Оптимизация: статика теперь на стороне Caddy, Python её не обрабатывает
-# app.mount("/static", StaticFiles(directory="app/static"), name="static")
-# os.makedirs("app/uploads", exist_ok=True)
-# app.mount("/media", StaticFiles(directory="app/uploads"), name="upload")
+# 2. ВЕРНУЛИ ЭТУ СТРОЧКУ! Теперь FastAPI сам гарантированно отдаст стили и скрипты
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 templates = Jinja2Templates(directory="app/templates")
 
 app.include_router(public.router)
 app.include_router(auth.router)
-# app.include_router(cms.router) 
 
-# Инициализация админки 
 setup_admin(app)
-
-if __name__ == "__main__":
-    # Для локальной разработки оставляем uvicorn, 
-    # но в Dockerfile для продакшена заменим на Gunicorn
-    uvicorn.run(
-        "app.main:app",
-        host="0.0.0.0",
-        port=8000,
-        forwarded_allow_ips='*',
-        reload=True
-    )
